145751d6bade4f9d153260a8f89462afab0382c7
raphealshirley <shirleyli8881@gmail.com>
2018-08-30 17:15:41 -0400

    Issue one

--- src/CommentInput.js
+++ src/CommentInput.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 
 class CommentInput extends Component{
+    static propTypes = {
+        onSubmit: PropTypes.func
+    }
+
     constructor(){
         super()
         this.state = {
@@ -10,10 +15,33 @@ class CommentInput extends Component{
         }
     }
 
+    _saveUsername(username){
+        localStorage.setItem('username',username)
+    }
+
+    _loadUsername(){
+        const username = localStorage.getItem('username')
+        if(username){
+            this.setState({username})
+        }
+    }
+
+    componentDidMount(){
+        this.textarea.focus()
+    }
+
+    componentWillMount(){
+        this._loadUsername()
+    }
+
     handleUsernameChange(event){
         this.setState({
             username:event.target.value
         })
+    }
+
+    handleUsernameBlur(event){
+        this._saveUsername(event.target.value)
     }
 
     handleContentChange(event){
@@ -38,6 +66,7 @@ class CommentInput extends Component{
                     <div className = 'comment-field-input'>
                     <input 
                         value = {this.state.username}
+                        onBlur = {this.handleUsernameBlur.bind(this)}
                         onChange = {this.handleUsernameChange.bind(this)} />  
                     </div>
                 </div>
@@ -45,6 +74,7 @@ class CommentInput extends Component{
                     <span className = 'comment-field-name'>Comment:</span>
                     <div className = 'comment-field-input'>
                         <textarea 
+                            ref = {(textarea) => this.textarea = textarea}
                             value = {this.state.content}
                             onChange = {this.handleContentChange.bind(this)}/>
                     </div>
